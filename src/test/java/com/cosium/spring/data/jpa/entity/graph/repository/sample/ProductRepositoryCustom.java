d4fa4ce28c79eaa022d370a8dbad8c00e5ff3fe9
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2016-11-28 09:58:26 +0100

    Don't fail on method returning void

---
+++ src/test/java/com/cosium/spring/data/jpa/entity/graph/repository/sample/ProductRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.cosium.spring.data.jpa.entity.graph.repository.sample;
+
+import com.cosium.spring.data.jpa.entity.graph.domain.EntityGraph;
+
+/**
+ * Created on 28/11/16.
+ *
+ * @author Reda.Housni-Alaoui
+ */
+public interface ProductRepositoryCustom {
+
+	void customMethod(EntityGraph entityGraph);
+
+}
---
+++ src/test/java/com/cosium/spring/data/jpa/entity/graph/repository/sample/ProductRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.cosium.spring.data.jpa.entity.graph.repository.sample;
+
+import com.cosium.spring.data.jpa.entity.graph.domain.EntityGraph;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created on 28/11/16.
+ *
+ * @author Reda.Housni-Alaoui
+ */
+@Component
+public class ProductRepositoryImpl implements ProductRepositoryCustom {
+
+	@Override
+	public void customMethod(EntityGraph entityGraph) {
+
+	}
+
+}
