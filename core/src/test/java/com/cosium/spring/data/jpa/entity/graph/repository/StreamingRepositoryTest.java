5b44b89e65cba0bdf9c0a82ba0bf75783a0dd1f2
w.kedziora <w.kedziora@ocado.com>
2023-02-10 14:35:27 +0100

    Support entity graphs in streaming methods

--- core/src/test/java/com/cosium/spring/data/jpa/entity/graph/repository/StreamingRepositoryTest.java
+++ core/src/test/java/com/cosium/spring/data/jpa/entity/graph/repository/StreamingRepositoryTest.java
@@ -3,12 +3,16 @@ package com.cosium.spring.data.jpa.entity.graph.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.cosium.spring.data.jpa.entity.graph.BaseTest;
+import com.cosium.spring.data.jpa.entity.graph.domain2.EntityGraph;
+import com.cosium.spring.data.jpa.entity.graph.domain2.NamedEntityGraph;
 import com.cosium.spring.data.jpa.entity.graph.sample.Product;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.inject.Inject;
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,13 +26,36 @@ class StreamingRepositoryTest extends BaseTest {
   @Inject private MyService myService;
 
   @Test
-  void test() {
+  void test1() {
     assertThat(myService.list("Product 1")).isNotEmpty();
+  }
+
+  @Test
+  @DisplayName("Given noop eg when finding products then brand should not be initialized")
+  void test2() {
+    List<Product> products = myService.list("Product 1", EntityGraph.NOOP);
+    assertThat(products).isNotEmpty();
+    for (Product product : products) {
+      assertThat(Hibernate.isInitialized(product.getBrand())).isFalse();
+    }
+  }
+
+  @Test
+  @DisplayName("Given brand eg when finding products then brand should be initialize")
+  void test3() {
+    List<Product> products =
+        myService.list("Product 1", NamedEntityGraph.loading(Product.BRAND_EG));
+    assertThat(products).isNotEmpty();
+    for (Product product : products) {
+      assertThat(Hibernate.isInitialized(product.getBrand())).isTrue();
+    }
   }
 
   interface ProductRepository extends EntityGraphRepository<Product, Long> {
 
     Stream<Product> findByName(String name);
+
+    Stream<Product> findByName(String name, EntityGraph entityGraph);
   }
 
   @Transactional
@@ -42,6 +69,10 @@ class StreamingRepositoryTest extends BaseTest {
 
     public List<Product> list(String name) {
       return productRepository.findByName(name).collect(Collectors.toList());
+    }
+
+    public List<Product> list(String name, EntityGraph entityGraph) {
+      return productRepository.findByName(name, entityGraph).collect(Collectors.toList());
     }
   }
 }
