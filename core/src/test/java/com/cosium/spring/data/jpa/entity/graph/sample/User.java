db2a5d630997a4dc33622ca23d69ca542ecef911
hugothomas <hugo.thomas98@gmail.com>
2023-12-13 15:17:08 +0100

    Enable composer for embeddable: review's corrections

--- core/src/test/java/com/cosium/spring/data/jpa/entity/graph/sample/User.java
+++ core/src/test/java/com/cosium/spring/data/jpa/entity/graph/sample/User.java
@@ -15,20 +15,4 @@ public class User {
   private long id = 0;
 
   private String login;
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getLogin() {
-    return login;
-  }
-
-  public void setLogin(String login) {
-    this.login = login;
-  }
 }
