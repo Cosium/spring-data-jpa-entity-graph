85988a6d834135fe0f5acc8793271d51b2ddd246
lassis <leassis@yahoo.com.br>
2023-12-25 17:13:52 +0100

    fix type information when using dinamic projection and generic repository

--- core/src/main/java/com/cosium/spring/data/jpa/entity/graph/repository/query/EntityGraphAwareJpaQueryMethod.java
+++ core/src/main/java/com/cosium/spring/data/jpa/entity/graph/repository/query/EntityGraphAwareJpaQueryMethod.java
@@ -1,27 +1,29 @@
 package com.cosium.spring.data.jpa.entity.graph.repository.query;
 
-import java.lang.reflect.Method;
 import org.springframework.data.jpa.provider.QueryExtractor;
-import org.springframework.data.jpa.repository.query.JpaParameters;
 import org.springframework.data.jpa.repository.query.JpaQueryMethod;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.query.Parameters;
+import org.springframework.data.repository.query.ParametersSource;
+
+import java.lang.reflect.Method;
 
 /**
  * @author Réda Housni Alaoui
  */
 class EntityGraphAwareJpaQueryMethod extends JpaQueryMethod {
 
-  protected EntityGraphAwareJpaQueryMethod(
-      Method method,
-      RepositoryMetadata metadata,
-      ProjectionFactory factory,
-      QueryExtractor extractor) {
-    super(method, metadata, factory, extractor);
-  }
+    protected EntityGraphAwareJpaQueryMethod(
+            Method method,
+            RepositoryMetadata metadata,
+            ProjectionFactory factory,
+            QueryExtractor extractor) {
+        super(method, metadata, factory, extractor);
+    }
 
-  @Override
-  protected JpaParameters createParameters(Method method) {
-    return new EntityGraphAwareJpaParameters(method);
-  }
+    @Override
+    protected Parameters<?, ?> createParameters(ParametersSource parametersSource) {
+        return new EntityGraphAwareJpaParameters(parametersSource);
+    }
 }
