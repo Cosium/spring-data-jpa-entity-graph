5dc857c2cf853667bb65876e448a0bd175b79119
dmarotta77 <37588099+dmarotta77@users.noreply.github.com>
2025-05-24 10:12:22 +0200

    Upgrade to Spring Data JPA 3.5.0 (#253)

--- core/src/main/java/com/cosium/spring/data/jpa/entity/graph/repository/query/EntityGraphAwareJpaQueryMethod.java
+++ core/src/main/java/com/cosium/spring/data/jpa/entity/graph/repository/query/EntityGraphAwareJpaQueryMethod.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.provider.QueryExtractor;
 import org.springframework.data.jpa.repository.query.JpaQueryMethod;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.core.RepositoryMetadata;
-import org.springframework.data.repository.query.Parameters;
-import org.springframework.data.repository.query.ParametersSource;
 
 /**
  * @author Réda Housni Alaoui
@@ -18,11 +16,6 @@ class EntityGraphAwareJpaQueryMethod extends JpaQueryMethod {
       RepositoryMetadata metadata,
       ProjectionFactory factory,
       QueryExtractor extractor) {
-    super(method, metadata, factory, extractor);
-  }
-
-  @Override
-  protected Parameters<?, ?> createParameters(ParametersSource parametersSource) {
-    return new EntityGraphAwareJpaParameters(parametersSource);
+    super(method, metadata, factory, extractor, EntityGraphAwareJpaParameters::new);
   }
 }
