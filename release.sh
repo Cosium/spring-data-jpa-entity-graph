fcfe53f26352712325fb7f035a4f54ebed4aea0e
Réda Housni Alaoui <reda-alaoui@hey.com>
2025-12-30 15:48:58 +0100

    Fix release script

--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# Prime gpg-agent caches. See https://maven.apache.org/plugins/maven-gpg-plugin/usage.html
+echo "test" | gpg --clearsign > /dev/null
 ./mvnw --batch-mode clean release:prepare release:perform && git push && git push --tags
